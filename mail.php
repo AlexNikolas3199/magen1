fad7452a16cf6586905646c40973aa44487a058a
AlexNikolas3199 <50858440+AlexNikolas3199@users.noreply.github.com>
2019-06-14 14:40:07 +0930

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	 <?php
+	 	mail("magen.sakha@gmail.com","Показания от ", message)
+	 ?>
+</body>
+</html>
